72c63bf04d333651efeacd5b7c1c9e507f863a4b
AsalGrg <143089968+AsalGrg@users.noreply.github.com>
2024-02-17 23:56:08 +0545

    add event bugs fixes

--- src/main/java/com/backend/controllers/EventController.java
+++ src/main/java/com/backend/controllers/EventController.java
@@ -10,17 +10,23 @@ import com.backend.services.EventService;
 import com.backend.utils.IsImage;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 
 @RestController
 @Validated
 @CrossOrigin("*")
+
+@Slf4j
 //@RequestMapping("/events")
 public class EventController {
 
@@ -78,18 +84,26 @@ public class EventController {
                                       @IsImage @RequestPart(name = "eventCoverImage") MultipartFile eventCoverImage,
                                       @Valid @RequestPart(name = "eventTicketDetails")EventTicketDetailsDto eventTicketDetailsDto,
                                       @Valid @RequestPart(name = "eventStarringDetails", required = false) EventStarringDetails eventStarringDetails,
-                                      @RequestPart(name = "starring1Photo", required = false) @IsImage MultipartFile starring1Photo,
-                                      @RequestPart(name = "starring2Photo", required = false) @IsImage MultipartFile starring2Photo,
-                                      @RequestPart(name = "starring3Photo", required = false) @IsImage MultipartFile starring3Photo,
-                                      @RequestPart(name = "starring4Photo", required = false) @IsImage MultipartFile starring4Photo,
-                                      @RequestPart(name = "starring5Photo", required = false) @IsImage MultipartFile starring5Photo
-                                      ){
+                                      @RequestPart(name = "starring1Photo", required = false) MultipartFile starring1Photo,
+                                      @RequestPart(name = "starring2Photo", required = false) MultipartFile starring2Photo,
+                                      @RequestPart(name = "starring3Photo", required = false) MultipartFile starring3Photo,
+                                      @RequestPart(name = "starring4Photo", required = false) MultipartFile starring4Photo,
+                                      @RequestPart(name = "starring5Photo", required = false) MultipartFile starring5Photo
+                                      ) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 
         addEventDto.setEventCoverPhoto(eventCoverImage);
 
 //        return new ResponseEntity<>(eventService.addEvent(addEventDto,eventTicketDetailsDto, eventDateDetailsDto), HttpStatus.OK);
-
-        return  ResponseEntity.ok("Donneeeee");
+        MultipartFile[] multipartFiles= {starring1Photo, starring2Photo, starring3Photo, starring4Photo, starring5Photo};
+        for (int i =0; i<multipartFiles.length; i++){
+            if(multipartFiles[i]!=null){
+                 Method method= eventStarringDetails.getClass().getMethod("setStarring" + (i + 1) + "Photo", MultipartFile.class);
+                method.invoke(eventStarringDetails, multipartFiles[i]);
+            }
+        }
+        return new ResponseEntity<>(eventService.addEvent(addEventDto,eventTicketDetailsDto, eventDateDetailsDto, eventStarringDetails), HttpStatus.OK);
+//        log.info("dsldld"+ eventStarringDetails.getStarring1Name());
+//        return  ResponseEntity.ok("");
     }
     @PostMapping( path = "/check"
     ,consumes = {"multipart/form-data"})
--- src/main/java/com/backend/services/StarringService.java
+++ src/main/java/com/backend/services/StarringService.java
@@ -1,13 +1,15 @@
 package com.backend.services;
 
 import com.backend.dtos.AddStarringDto;
+import com.backend.dtos.addEvent.EventStarringDetails;
 import com.backend.models.Event;
 import com.backend.models.EventStarring;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public interface StarringService {
 
-    void saveStarring(List<MultipartFile> starringsPhotos, List<String> starringNames, Event event);
+    void saveStarring( EventStarringDetails eventStarringDetails, Event event);
 }
